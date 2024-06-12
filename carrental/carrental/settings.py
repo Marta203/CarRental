1dc314ae99a1acdb051d425c387f3f1a6cfed544
Marta203 <martusia.stuglik@gmail.com>
2024-06-12 11:05:32 +0200

    2024-05-08 MS

--- carrental/carrental/settings.py
+++ carrental/carrental/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(fcywl^tn!r*$0xb))7xng10xz_b2am__kelgfk&5j8ypu+cp2'
+SECRET_KEY = 'django-insecure-0m322r946h3(iu1jpm+ei)6!n!9#$vj_xz&5@%wc%g4r9c7w)m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
