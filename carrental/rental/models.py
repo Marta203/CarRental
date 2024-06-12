6e0ba2b315258bfbfddd48a439784be7257aecd5
Marta203 <martusia.stuglik@gmail.com>
2024-06-12 11:06:47 +0200

    2024-05-15 MS

--- carrental/rental/models.py
+++ carrental/rental/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class Equipment(models.Model):
     equipment = models.CharField(max_length=50)
@@ -25,11 +27,30 @@ class Car(models.Model):
     engine_power = models.PositiveSmallIntegerField()
     color = models.CharField(max_length=20)
     equipment = models.ManyToManyField(Equipment)
-    gearbox_type = models.CharField(max_length=20, choices=ENGINE_TYPES)
+    gearbox_type = models.CharField(max_length=20, choices=GEARBOX_TYPES)
     available = models.BooleanField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
 
-class User(models.Model):
-    pass
+class Address(models.Model):
+    user = models.OneToOneField(User, on_delete=models.RESTRICT)
+    country = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    post_code = models.CharField(max_length=10)
+    street = models.CharField(max_length=50)
+    building_no = models.CharField(max_length=10)
+    appartment_no = models.CharField(max_length=10)
+
+class User(User):
+    IDENTITY_DOCUMENT_TYPES = [
+        ("dowod", "dowód osobisty"),
+        ("paszport", "paszport"),
+        ("prawo_jazdy", "prawo jazdy")
+    ]
+    phone = models.CharField(max_length=20)
+    identity_document_type = models.CharField(max_length=20, choices=IDENTITY_DOCUMENT_TYPES)
+    identity_document_no = models.CharField(max_length=20)
+    # address = models.OneToOneField(Address, on_delete=models.RESTRICT)
 
 class Order(models.Model):
     PAYMENT_METHODS = [
@@ -47,4 +68,3 @@ class Order(models.Model):
     deposit = models.DecimalField(max_digits=10, decimal_places=2)
     payment_method = models.CharField(max_length=20, choices=PAYMENT_METHODS)
     payment_status = models.BooleanField()
-
